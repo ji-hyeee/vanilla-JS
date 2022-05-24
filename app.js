1e00f62571f6190b548e44c011159a42ae147701
sulmeee <poorooria@naver.com>
2022-05-24 23:18:00 +0900

    learn variable and const, let

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 alert("hi");
+
+// variable
+const a = 5;
+const b = 2;
+
+let myName = "hye";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+// value update
+myName = "JiHye";
+console.log("your new name is " + myName);
+
+// always const
+// sometimes let
+// never var
