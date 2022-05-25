3c1532c46a5c18806bc5ff5d40c3f4ac78d2cb55
sulmeee <poorooria@naver.com>
2022-05-25 22:21:33 +0900

    docs: learn new type

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
-alert("hi");
+// boolean
+// true or false
 
-// variable
-const a = 5;
-const b = 2;
+const amIFat = true; // !== "true"
+console.log(amIFat);
 
-let myName = "hye";
+// 언제 사용할까?
+// 예) 사용자가 로그인 했는가?
+// 예) 웹사이트가 로딩되고 있는가?
 
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
+// unll
+// 아무것도 없음 / 변수에 아무것도 없음
+// null !== false >>> false라는 값이 있는 것
+// null은 자연적으로 발생하지 않는다
+// 변수 안에 어떤 것이 없다는 것을 확실하기 위해 쓴다
+// 자바스크립트에 값이 없다라는 것을 알려주기 위해서
 
-// value update
-myName = "JiHye";
-console.log("your new name is " + myName);
+// undefined
+// 변수의 값을 할당하지 않은 경우
+// 변수가 존재하지만 값이 존재하지 않는 것
+let something
 
-// always const
-// sometimes let
-// never var
+// 두번째로 중요한 타입을 배웠다
+// true, false, null, undefined
+// true === on
+// false === off
+// null === nothing
+// undefinde === yes variable no value
